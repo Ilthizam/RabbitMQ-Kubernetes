e7d4bf8ecc6337464518fd95f786ed30c1c69d8e
Ilthizam Imtiyas <ilthizam.imtiyas@ifsworld.com>
2021-03-25 08:56:25 +0530

    rabbitmq publisher get queryparms

--- py-applications/publisher/app/publisher.py
+++ py-applications/publisher/app/publisher.py
@@ -7,8 +7,8 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/', methods=['GET'])
-def home():
+@app.route('/<task_name>', methods=['GET'])
+def home(task_name):
     
     credentials = pika.PlainCredentials("guest", "guest")
     # connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
@@ -17,7 +17,7 @@ def home():
     channel.queue_declare(queue='hello')
     channel.basic_publish(exchange='',
                       routing_key='hello',
-                      body='test!')
+                      body=task_name)
                       
     print(" [x] Sent 'Hello World!'")
 
