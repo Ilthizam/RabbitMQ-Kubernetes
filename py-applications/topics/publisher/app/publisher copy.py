a7ba8c3f5a29694383eb50ba5fdf65ce4fab60f2
Ilthizam Imtiyas <ilthizam.imtiyas@ifsworld.com>
2021-04-12 09:25:00 +0530

    rabbitmq topic

--- py-applications/topics/publisher/app/publisher copy.py
+++ py-applications/topics/publisher/app/publisher copy.py
@@ -14,7 +14,9 @@ def home(task_name):
     # connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq-0.rabbitmq.rabbits.svc.cluster.local", "5672", '/', credentials ))
     channel = connection.channel()
+
     channel.queue_declare(queue='hello')
+    
     channel.basic_publish(exchange='',
                       routing_key='hello',
                       body=task_name)
