69daa55b75a676a2066426f538bef7b7b40a6c14
Ilthizam Imtiyas <ilthizam.imtiyas@ifsworld.com>
2021-04-23 12:05:29 +0530

    rabbitmq topics with flask

--- py-applications/topics/publisher/app/publisher.py
+++ py-applications/topics/publisher/app/publisher.py
@@ -1,18 +1,40 @@
 #!/usr/bin/env python
 import pika
 import sys
+import flask
+from flask import request
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
 
-channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
 
-routing_key = sys.argv[1] if len(sys.argv) > 2 else 'anonymous.info'
-message = ' '.join(sys.argv[2:]) or 'Hello World!'
 
-channel.basic_publish(
-    exchange='topic_logs', routing_key=routing_key, body=message)
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+ 
 
-print(" [x] Sent %r:%r" % (routing_key, message))
-connection.close()
+@app.route('/run', methods=['POST'])
+def run():
+
+
+    credentials = pika.PlainCredentials("guest", "guest")
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters("rabbitmq-0.rabbitmq.rabbits.svc.cluster.local", "5672", '/', credentials ))
+
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
+
+    routing_key = request.json['namespace']
+    message = request.json['command']
+
+    channel.basic_publish(
+        exchange='topic_logs', routing_key=routing_key, body=message)
+
+    print(" [x] Sent %r:%r" % (routing_key, message))
+
+    connection.close()
+
+    return "<h1>RabitMQ working</p>"
+
+    
+app.run()
